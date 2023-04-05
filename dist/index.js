9c0e6c7d106b8e0440c6611a1c9a905819543901
Gary Cruise <Garycruisee@gmail.com>
2023-04-05 22:03:46 +0700

    add api

--- dist/index.js
+++ dist/index.js
@@ -1,18 +1,28 @@
 const usersUrl = "https://642556059e0a30d92b2f08bd.mockapi.io/users/";
 
-// Sign In
+async function loginBtn() {
+  const username = document.getElementById("email-sign").value;
+  console.log(username);
+  const userPassword = document.getElementById("password-sign").value;
+  console.log(userPassword);
 
-function loginBtn() {
-  var storedName = localStorage.getItem("email");
-  var storedPw = localStorage.getItem("password");
-
-  var userName = document.getElementById("email-sign");
-  var userPw = document.getElementById("password-sign");
-
-  if (userName.value == storedName && userPw.value == storedPw) {
-    window.location.href = "homepage.html";
-    alert("You are logged in.");
+  if (!username) {
+    alert("Please input your username");
+  } else if (!userPassword) {
+    alert("Please input your password");
   } else {
-    alert("Error on login");
+    try {
+      const response = await fetch(usersUrl);
+      const user = await response.json();
+      if (user[0].password == userPassword) {
+        alert("success!");
+        window.location.href = "homepage.html";
+      } else {
+        alert("Invalid username or password!");
+      }
+    } catch (error) {
+      console.log(error);
+      alert("Invalid username or password!");
+    }
   }
 }
--- dist/signup.js
+++ dist/signup.js
@@ -1,23 +1,38 @@
 const usersUrl = "https://642556059e0a30d92b2f08bd.mockapi.io/users/";
-const usersData = localStorage.getItem("users");
-const users = JSON.parse(usersData) || [];
-localStorage.setItem("users", JSON.stringify(users));
-
 // Sign Up
 
-function signupBtn() {
-  const emailInput = document.getElementById("email-input");
-  const passwordInput = document.getElementById("password-input");
+async function signupBtn() {
+  const emailReg = document.getElementById("email-input").value;
+  console.log(emailReg);
+  const passwordReg = document.getElementById("password-input").value;
+  console.log(passwordReg);
 
-  if (emailInput.value.length == 0) {
-    alert("Please fill in email");
-  } else if (passwordInput.value.length == 0) {
-    alert("Please fill in password");
-  } else if (emailInput.value.length == 0 && passwordInput.value.length == 0) {
-    alert("Please fill in email and password");
+  if (!usernameReg) {
+    alert("Please input your username");
+  } else if (!emailReg) {
+    alert("Please input your email");
+  } else if (!passwordReg) {
+    alert("Please input your password");
   } else {
-    localStorage.setItem("email", emailInput.value);
-    localStorage.setItem("password", passwordInput.value);
-    alert("Your account has been created");
+    try {
+      const response = await fetch(usersUrl, {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          usernameReg,
+          emailReg,
+          passwordReg: rePasswordReg,
+        }),
+      });
+      const newUser = await response.json();
+      alert(`Successfully registered with ID ${newUser.id}`);
+      // Redirect to login page after successful registration
+      window.location.href = "index.html";
+    } catch (error) {
+      console.log(error);
+      alert("Error registering user");
+    }
   }
 }
